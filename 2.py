626859ab80177e14442f08fac1f27495ef939a37
sayeesh <sayeeshpatil64@gmail.com>
2026-01-17 11:49:59 +0530

    save

---
+++ 2.py
@@ -0,0 +1,6 @@
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return (
+        f"Celsius: {celsius}\n"
+        f"Fahrenheit: {fahrenheit}"
+    )
---
+++ test_2.py
@@ -0,0 +1,6 @@
+from test_2 import celsius_to_fahrenheit
+
+def test_temperature():
+    result = celsius_to_fahrenheit(0)
+    expected = "Celsius: 0\nFahrenheit: 32.0"
+    assert result == expected
